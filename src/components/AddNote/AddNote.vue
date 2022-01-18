4a715f0aa3260960eef547f86c320281d9ba0aff
Ali Furkan Yaman <alifurkanymn@gmail.com>
2022-01-18 16:27:54 +0300

    edited

--- src/components/AddNote/AddNote.vue
+++ src/components/AddNote/AddNote.vue
@@ -2,30 +2,50 @@
     <div class="addNote">
         <div class="inputs">
             <h3>Title:</h3>
-            <input type="text">
+            <input v-model="note.title" type="text">
         </div>
         <div class="textArea">
             <h3>Content:</h3>
-            <textarea></textarea>
+            <textarea v-model="note.content"></textarea>
         </div>
         <div class="comboBox">
             <h3>Materiality Level</h3>
-            <select name="Important">
+            <select v-model="note.options" name="Important">
             <option class="option" value="Important">Important</option>
             <option class="option" value="Normal">Normal</option>
             <option class="option" value="Unhurried">Unhurried</option>
             </select>
         </div>
         <div class="button">
-            <button>Add</button>
+            <button @click="onSubmit()" type="submit">Add</button>
         </div>
     </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   data(){
     return{
+        note : {
+            title : "",
+            content : "",
+            options : "",
+        }
     }
+  },
+  methods : {
+      onSubmit(){
+          axios.post(`https://vue-note-app-be493-default-rtdb.firebaseio.com/note.json`,{
+              ...this.note,
+          })
+          .then(response => {
+              console.log(response);
+          })
+          .catch(e => {
+              console.log(e)
+            })
+      }
   }
 }
 </script>
